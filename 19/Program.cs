db68342364ed702cf4feb71800bfdd8c0f8756ef
fake_state <ntdako@gmail.com>
2023-02-19 22:03:35 +0400

    add 19 and gitignore

---
+++ 19/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на 
+вход пятизначное число и проверяет, является ли оно палиндромом.*/
+
+int Number() // Ввод числа
+{
+    Console.WriteLine("Введите число и я скажу полиндром ли это: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+bool range(int N) // Проверка корректного ввода
+{
+    if (N > 9999 && N < 100000) return true;
+    else return false;
+}
+
+int N = Number();
+if (range(N))
+{
+    if(N/10000 == N % 10 && (N/1000)%10 == (N%100)/10)
+    {
+        Console.WriteLine("Да, это палиндром");
+    }
+    else Console.WriteLine("Нет, это не палиндром");
+} 
+else Console.WriteLine("Число не пятизначное!");
