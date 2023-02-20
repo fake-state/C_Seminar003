f2a4431a51a6e95e60268e4eec6bedef833907b3
fake_state <ntdako@gmail.com>
2023-02-20 13:45:56 +0400

    add 21 and 23

---
+++ 21/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход координаты 
+двух точек и находит расстояние между ними в 3D пространстве.*/
+
+void Dots (int[] X) // Ввод координат
+{
+    Console.WriteLine("Поочередно введите x, y, z: ");
+    Console.Write("x =" + " ");
+    X[0] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("y =" + " ");
+    X[1] = Convert.ToInt32(Console.ReadLine());
+    Console.Write("z =" + " ");
+    X[2] = Convert.ToInt32(Console.ReadLine());
+}
+
+double Lon(int[] A, int[] B) // Рассчет расстояния между точками в 3Д
+{
+double lon = Math.Sqrt(Math.Pow((B[0] - A[0]), 2) + Math.Pow((B[1] - A[1]), 2) + Math.Pow((B[2] - A[2]), 2));
+return lon;
+}
+
+int[] A = new int[3];
+int[] B = new int[3];
+
+Console.WriteLine("Введите координаты первой точки.");
+Dots(A);
+Console.WriteLine("Введите координаты второй точки.");
+Dots(B);
+
+Console.WriteLine($"Расстояние между точками: {Lon(A, B)}");
+
---
+++ 23/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает 
+на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+
+
+int N() // Ввод числа
+{
+    Console.WriteLine("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+void Cube(int number)
+{
+    for (int i = 1; i <= number ; i++)
+    {
+        int cube = i*i*i;
+        Console.Write($"{cube}  ");
+    }
+}
+
+int n = N();
+Console.WriteLine($"Кубы от 1 до {n}: ");
+Cube(n);
